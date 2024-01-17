e08fb954e8552928cccb90b286f510bafcd04a51
idalxdd <jubaacioly@gmail.com>
2024-01-17 17:42:17 -0300

    prepare for deploy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import { app } from './app'
-import { env } from './env'
+import { app } from "./app";
+import { env } from "./env";
 
 app
   .listen({
     port: env.PORT,
+    host: "RENDER" in process.env ? `0.0.0.0` : `localhost`,
   })
   .then(() => {
-    console.log('ta rodando kkk')
-  })
+    console.log("ta rodando kkk");
+  });
